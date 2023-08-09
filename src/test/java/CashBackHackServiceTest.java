c7e3fc2df40a85ef3ef4ce52c4dcb6c9ce80c483
Maksim T <verdo.ya.ry19@gmail.com>
2023-08-09 22:15:34 +0400

    Test NG

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -1,2 +1,36 @@
+package ru.netology.maven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashBackHackServiceTest {
+    @Test
+    void onLimit() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    void underLimit() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    void secondBonus() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
 }
